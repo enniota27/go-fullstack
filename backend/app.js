9c6b02acc20ef75e55bdee51d650f2807db0892e
Antoine NAGY <nagy.antoine@gmail.com>
2020-06-22 17:37:07 +0200

    Connexion à la base de donnée

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require('express'); //Import express
 const bodyParser = require('body-parser'); //Import body-parser pour extraire des objets JSON en JS
+const mongoose = require('mongoose'); // Import mongosse pour la base de donnée
 
 const app = express(); //Permet de créer une application express
+
+mongoose.connect('mongodb+srv://Antoine27:mdp27@cluster0.5arjs.mongodb.net/<dbname>?retryWrites=true&w=majority', // Connection à la base de donnée
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => { //Ajoute CORS dans l'entête de toutes les requêtes
     res.setHeader('Access-Control-Allow-Origin', '*'); // Permet d'accéder à notre API depuis n'importe quelle origine = '*'
