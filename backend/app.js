71a8d2926502695675a28d9c303b8bd179852718
Antoine NAGY <nagy.antoine@gmail.com>
2020-06-22 16:56:23 +0200

    Création d'une route GET

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,33 @@ const express = require('express'); //Import express
 
 const app = express(); //Permet de créer une application express
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-  
-app.use((req, res, next) => {
-    res.status(201); //Modifie le code HTTP
+app.use((req, res, next) => { //Ajoute CORS dans l'entête de toutes les requêtes
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permet d'accéder à notre API depuis n'importe quelle origine = '*'
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //Verbe que l'on autorise
     next();
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/stuff', (req, res, next) => { // '/api/stuff' = url visé par l'appli
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff); // Attribue un code 200 à la réponse
+  });
 
 module.exports = app; //Exporter la constante app pour y avoir accèder depuis les autres fichiers
