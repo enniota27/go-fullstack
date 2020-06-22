b10f142a014772fcc23e635d6f25256fbf180420
Antoine NAGY <nagy.antoine@gmail.com>
2020-06-22 18:30:16 +0200

    Enregistrer et récupérer les données de la base

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express'); //Import express
 const bodyParser = require('body-parser'); //Import body-parser pour extraire des objets JSON en JS
 const mongoose = require('mongoose'); // Import mongosse pour la base de donnée
 
+const Thing = require('./models/Thing'); // Import le modèle Thing
+
 const app = express(); //Permet de créer une application express
 
 mongoose.connect('mongodb+srv://Antoine27:mdp27@cluster0.5arjs.mongodb.net/<dbname>?retryWrites=true&w=majority', // Connection à la base de donnée
@@ -20,32 +22,27 @@ app.use((req, res, next) => { //Ajoute CORS dans l'entête de toutes les requêt
 app.use(bodyParser.json()); // Tranforme le corps de la requête en objet JS pour toutes les routes
 
 app.post('/api/stuff', (req, res, next) => { // POST
-    console.log(req.body); //Corps de la requête
-    res.status(201).json({
-      message: 'Objet créé !'
+    delete req.body._id; // Supprime l'identifiant renvoyer par le frontend
+    const thing = new Thing({ // Créer une nouvelle instance = créer un objet
+        ...req.body //Copie les champs de la body de la request
     });
-  });
+    thing.save() // Enregistre l'objet dans la base (promesse)
+        .then(() => res.status(201).json({ message : 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+});
 
+// Récupérer un Thing spécifique
+app.get('/api/stuff/:id', (req, res, next) => { // ':' rendre accessible en tant que paramètre
+    Thing.findOne({ _id: req.params.id }) // Récupérère que l'objet dont l'id est la même que l'url de la route
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+});
+
+// Récupérer la liste des Things
 app.use('/api/stuff', (req, res, next) => { // '/api/stuff' = url visé par l'appli
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff); // Attribue un code 200 à la réponse
-  });
+    Thing.find()
+        .then(things => res.status(200).json(things)) // Récupére le tableau de tous les Things de la base
+        .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app; //Exporter la constante app pour y avoir accèder depuis les autres fichiers
